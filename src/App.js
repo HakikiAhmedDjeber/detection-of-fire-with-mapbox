f82dd81198128f9dfe5ff66d9b2944e0f3118365
HakikiAhmedDjeber <hakikiahmeddjeber@gmail.com>
2024-01-28 17:39:29 +0100

    first setup with changing the default marker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import L from "leaflet";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer
+        center={[35.2, -0.641389]}
+        zoom={15}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[35.2, -0.641389]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[35.2, -0.641389]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[35.2, -0.641389]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[35.2, -0.641389]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
-
+let DefaultIcon = L.icon({
+  iconUrl: "sensor.png",
+  iconSize: [20, 20],
+});
+console.log(L.marker);
+L.Marker.prototype.options.icon = DefaultIcon;
 export default App;
