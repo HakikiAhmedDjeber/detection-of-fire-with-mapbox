4fd68dbce724eb438c000b775ee8cd47404547f0
HakikiAhmedDjeber <hakikiahmeddjeber@gmail.com>
2024-02-17 10:40:41 +0100

    fix markers

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Header from "./components/Home/Header";
-import "./App.css";
-import Map from "./components/Home/Map";
-import Sidebar from "./components/Home/Sidebar";
-import { useSubscriptionContext } from "./Context/SubscriptionContext";
-import Chart from "./components/Home/Chart";
-import { useLazyQuery } from "@apollo/client";
-import {
-  GET_ALL_SENSSED_DATA,
-  GET_ALL_SENSSED_DATA_BY_DEVICE,
-} from "./GraphQL/queries";
-
 // import for routes
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import History from "./components/History/History";
 
 export default function App() {
-  const [SensorData, setSensorData] = useState(null);
-  const [regionName, setRegionName] = useState("");
-  const [isChartOpen, setIsChartOpen] = useState(false);
-  const { incomingData, loading, error } = useSubscriptionContext();
-
-  const [
-    getAllSenssedData,
-    { loading: isLoading, data: responseData, error: queryError },
-  ] = useLazyQuery(GET_ALL_SENSSED_DATA); //  ADD new this will fetch allll historic
-  const [
-    getAllSenssedDataByDevice,
-    { loading: isDLoading, data: DeviceResponseData, error: queryDeviceError },
-  ] = useLazyQuery(GET_ALL_SENSSED_DATA_BY_DEVICE); //  ADD new this will fetch all data by deviceID
-
-  useEffect(() => {
-    getAllSenssedData(); // fetching all records data of all sensors from backend database
-
-    getAllSenssedDataByDevice({ variables: { deviceId: "123" } }); // excute fetch data only for single device
-  }, []);
-
-  useEffect(() => {
-    if (!isLoading) {
-      if (queryError) {
-        console.log(queryError);
-      }
-      if (responseData) {
-        console.log("Recieved Data ==> ", responseData);
-      }
-    }
-  }, [isLoading, responseData, queryError]);
-
-  useEffect(() => {
-    if (!isDLoading) {
-      if (queryDeviceError) {
-        console.log(queryDeviceError);
-      }
-      if (DeviceResponseData) {
-        console.log(
-          "Recieved Data from single device ==> ",
-          DeviceResponseData
-        );
-      }
-    }
-  }, [isDLoading, DeviceResponseData, queryDeviceError]);
-
-  function handleChartOpen() {
-    setIsChartOpen(!isChartOpen);
-  }
-  function handleRegionName(name) {
-    setRegionName(name);
-  }
-
-  // viewport
-  const [viewport, setViewport] = useState({
-    longitude: -0.41551,
-    latitude: 35.20779,
-    zoom: 12,
-  });
-
-  function handleViewport(view) {
-    setViewport(view);
-  }
-  useEffect(() => {
-    if (incomingData) {
-      let temp = incomingData?.data?.subscriptionTest;
-      setSensorData(temp);
-      console.log(temp);
-      console.log(typeof temp);
-    }
-  }, [incomingData]);
-
   return (
     <div className="app">
       <Router>
