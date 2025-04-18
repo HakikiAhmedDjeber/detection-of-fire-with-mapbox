319c28b68470c1a78c51fe83e599f4e386adc9ca
HakikiAhmedDjeber <hakikiahmeddjeber@gmail.com>
2025-04-18 15:28:38 +0100

    change the redis

--- api/src/graphql/utils/pubsub.js
+++ api/src/graphql/utils/pubsub.js
@@ -2,15 +2,14 @@ const { RedisPubSub } = require("graphql-redis-subscriptions");
 
 const pubsub = new RedisPubSub({
   connection: {
-    password: 'JXagbpiG6Kj0Opmdc1hIPuY886vUMeCl',
-    host: "redis-16389.c56.east-us.azure.cloud.redislabs.com",
-    port: 16389,
+    password: "mCbs9TRAlzgUvx9zXEnJRqCAZXZCpEd6",
+    host: "redis-15399.c73.us-east-1-2.ec2.redns.redis-cloud.com",
+    port: 15399,
     // password: 'pass*',
     retryStrategy: (times) => {
       return Math.min(times * 50, 2000);
     },
   },
 });
-
 
 module.exports = pubsub;
